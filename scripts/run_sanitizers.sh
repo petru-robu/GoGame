d617d20565e865fcfb1ed5539ca1631cb800168d
Petru Robu <petru.robu@hotmail.com>
2025-05-20 12:59:09 +0300

    almost done

--- scripts/run_sanitizers.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-INPUT_FILENAME=${INPUT_FILENAME:-tastatura.txt}
-ZIP_NAME=${ZIP_NAME:-install_dir/bin}
-EXECUTABLE_NAME=${EXECUTABLE_NAME:-oop}
-
-tr -d '\r' < "${INPUT_FILENAME}" | ./"${ZIP_NAME}"/"${EXECUTABLE_NAME}" &
-bash ./scripts/run_test.sh 3 1 2
